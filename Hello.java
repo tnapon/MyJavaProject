cd1e6afd9d3d560d0614980cf5123990f5dd3db0
tnapon <614259035@webmail.npru.ac.th>
2020-08-20 12:03:02 +0700

    edit testinng

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("SE");
-		System.out.println("Hello SE");
+		System.out.println("SE");
 		System.out.println("Hello SE");
 	}
 }
